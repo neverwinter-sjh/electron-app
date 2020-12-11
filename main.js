79176b49128e0105b43492d09a6d3d060dd0172b
neverwintersjh <neverwinter.sjh@gmail.com>
2020-12-11 17:46:05 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const Store = require('./utils/store');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('./build/index.html');
+
+  // dev tool 열기
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const App = () => {
+  const [data, getData] = useState([]);
+  const [todos, setTodo] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos').then(r => {
+      console.log(r);
+      // getData(r.data);
+      setTimeout(() => {
+        getData(r.data);
+      }, 3000);
+    });
+  }, []);
+
+  const setText = e => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      <h1>Electron React App</h1>
+      <div>
+        내용 수정을 위해서 리액트 내용을 수정하고 일렉트론 브라우저 새로고침을 하면 된다.
+        <br />
+        F5 or Ctrl + r을 누른다.
+      </div>
+      <hr />
+      <h2>로컬에 저장되는 Todos</h2>
+      <p>
+        <input onChange={setText} />
+        <button type="button" onClick={setTodos}>
+          입력
+        </button>
+      </p>
+      {todos.length === 0 && <p>현재 할일이 없습니다.</p>}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo.text}</li>
+        ))}
+      </ul>
+      <hr />
+      <h2>https://jsonplaceholder.typicode.com/todos</h2>
+      {data.length === 0 && <p>Loading...</p>}
+      {data.map(item => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
